c9fb85c1319cf45d85004c50f7716cf3828f5e8f
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-14 01:03:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,8 +66,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
